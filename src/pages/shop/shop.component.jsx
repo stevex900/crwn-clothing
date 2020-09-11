e75c733b987c028ef319312c1ded9d509506043f
Adam Stefaniak <a.stefaniak900@gmail.com>
2020-09-11 16:30:00 +0200

    updated shop data  with new data, updated cart utl;s with reducer

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,11 +1,18 @@
 import React from "react";
-import { SHOP_DATA } from "./shop.data";
+
+import SHOP_DATA from "./shop.data.js";
+
 import CollectionPreview from "../../components/collection-preview/collection-preview.component";
+
 class ShopPage extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { collections: SHOP_DATA };
+
+    this.state = {
+      collections: SHOP_DATA,
+    };
   }
+
   render() {
     const { collections } = this.state;
     return (
